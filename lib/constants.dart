b50d59d90e18c76655f6d606283b0d0060b0fc66
tahagiremle <tahagiremle@gmail.com>
2024-10-18 15:22:33 +0300

    add Constants class

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+class Constants {
+  static const Map<int, String> categorys = {
+    0: "General",
+    1: "Roman",
+    2: "Story",
+    3: "Education",
+    4: "Fantastic",
+    5: "Science fiction",
+    6: "Biagrophy"
+
+
+
+  };
+}
--- lib/view/chapter_detail_page.dart
+++ lib/view/chapter_detail_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_author_app/database/local_database.dart';
 import 'package:flutter_author_app/model/chapter.dart';
